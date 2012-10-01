3b8d6c655d6c45ebbb21cd142d3eec290364485d
Russ Cox <rsc@golang.org>
2012-10-01 16:03:10 -0400

    plan9/client: implement real namespace lookup

--- plan9/client/dial.go
+++ plan9/client/dial.go
@@ -1,8 +1,11 @@
 package client
 
 import (
+	"fmt"
 	"net"
 	"os"
+	"regexp"
+	"strings"
 )
 
 func Dial(network, addr string) (*Conn, error) {
@@ -14,10 +17,7 @@ func Dial(network, addr string) (*Conn, error) {
 }
 
 func DialService(service string) (*Conn, error) {
-	ns := os.Getenv("NAMESPACE")
-	if ns == "" {
-		return nil, Error("unknown name space")
-	}
+	ns := Namespace()
 	return Dial("unix", ns+"/"+service)
 }
 
@@ -43,4 +43,33 @@ func MountService(service string) (*Fsys, error) {
 		c.Close()
 	}
 	return fsys, err
+}
+
+var dotZero = regexp.MustCompile(`\A(.*:\d+)\.0\z`)
+
+// Namespace returns the path to the name space directory.
+func Namespace() string {
+	ns := os.Getenv("NAMESPACE")
+	if ns != "" {
+		return ns
+	}
+
+	disp := os.Getenv("DISPLAY")
+	if disp == "" {
+		// No $DISPLAY? Use :0.0 for non-X11 GUI (OS X).
+		disp = ":0.0"
+	}
+
+	// Canonicalize: xxx:0.0 => xxx:0.
+	if m := dotZero.FindStringSubmatch(disp); m != nil {
+		disp = m[1]
+	}
+
+	// Turn /tmp/launch/:0 into _tmp_launch_:0 (OS X 10.5).
+	disp = strings.Replace(disp, "/", "_", -1)
+
+	// NOTE: plan9port creates this directory on demand.
+	// Maybe someday we'll need to do that.
+
+	return fmt.Sprintf("/tmp/ns.%s.%s", os.Getenv("USER"), disp)
 }
