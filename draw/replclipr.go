e50c18423448377ca4136a188184c98af3236a9d
Rob Pike <r@golang.org>
2012-12-18 17:34:42 -0800

    goplan9/draw: add ReplClipr Its absence was just an oversight.
    R=rsc
    CC=r, r, rsc, rsc
    https://codereview.appspot.com/6963044

---
+++ draw/replclipr.go
@@ -0,0 +1,20 @@
+package draw
+
+import "image"
+
+func (dst *Image) ReplClipr(repl bool, clipr image.Rectangle) {
+	b := dst.Display.bufimage(22)
+	b[0] = 'c'
+	bplong(b[1:], uint32(dst.ID))
+	byteRepl := byte(0)
+	if repl {
+		byteRepl = 1
+	}
+	b[5] = byteRepl
+	bplong(b[6:], uint32(clipr.Min.X))
+	bplong(b[10:], uint32(clipr.Min.Y))
+	bplong(b[14:], uint32(clipr.Max.X))
+	bplong(b[18:], uint32(clipr.Max.Y))
+	dst.Repl = repl
+	dst.Clipr = clipr
+}
