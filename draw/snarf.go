80e60ac54c501cf68d13fff4148ef436e612b8c5
Rob Pike <r@golang.org>
2013-08-11 09:15:36 +1000

    goplan9/draw: implement ReadSnarf and WriteSnarf It would be preferable if the RPC used bytes rather than strings, but I didn't want to mess with that.
    R=rsc
    CC=r, rsc
    https://codereview.appspot.com/12696047

--- draw/snarf.go
+++ draw/snarf.go
@@ -1,3 +1,24 @@
 package draw
 
-// TODO
+import (
+	"fmt"
+	"os"
+)
+
+func (d *Display) ReadSnarf() (string, error) {
+	str, err := d.conn.ReadSnarf()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ReadSnarf: %v\n", err)
+		return "", err
+	}
+	return str, nil
+}
+
+func (d *Display) WriteSnarf(str string) error {
+	err := d.conn.WriteSnarf(str)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "WriteSnarf: %v\n", err)
+		return err
+	}
+	return nil
+}
