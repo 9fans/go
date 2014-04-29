d01f1370ae774b3a1c85db211edf4ad5322497cd
Russ Cox <rsc@golang.org>
2014-04-29 13:41:51 -0400

    acme: add support for log file
    TBR=rsc
    CC=r, rsc
    https://codereview.appspot.com/93950043

--- plan9/acme/acme.go
+++ plan9/acme/acme.go
@@ -8,14 +8,15 @@ package acme
 import (
 	"bufio"
 	"bytes"
-	"code.google.com/p/goplan9/plan9"
-	"code.google.com/p/goplan9/plan9/client"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"strconv"
 	"strings"
 	"sync"
+
+	"code.google.com/p/goplan9/plan9"
+	"code.google.com/p/goplan9/plan9/client"
 )
 
 // A Win represents a single acme window and its control files.
@@ -77,6 +78,53 @@ func New() (*Win, error) {
 type WinInfo struct {
 	ID   int
 	Name string
+}
+
+// A LogReader provides read access to the acme log file.
+type LogReader struct {
+	f   *client.Fid
+	buf [8192]byte
+}
+
+func (r *LogReader) Close() error {
+	return r.f.Close()
+}
+
+// A LogEvent is a single event in the acme log file.
+type LogEvent struct {
+	ID   int
+	Op   string
+	Name string
+}
+
+// Read reads an event from the acme log file.
+func (r *LogReader) Read() (LogEvent, error) {
+	n, err := r.f.Read(r.buf[:])
+	if err != nil {
+		return LogEvent{}, err
+	}
+	f := strings.SplitN(string(r.buf[:n]), " ", 3)
+	if len(f) != 3 {
+		return LogEvent{}, fmt.Errorf("malformed log event")
+	}
+	id, _ := strconv.Atoi(f[0])
+	op := f[1]
+	name := f[2]
+	name = strings.TrimSpace(name)
+	return LogEvent{id, op, name}, nil
+}
+
+// Log returns a reader reading the acme/log file.
+func Log() (*LogReader, error) {
+	fsysOnce.Do(mountAcme)
+	if fsysErr != nil {
+		return nil, fsysErr
+	}
+	f, err := fsys.Open("log", plan9.OREAD)
+	if err != nil {
+		return nil, err
+	}
+	return &LogReader{f: f}, nil
 }
 
 // Windows returns a list of the existing acme windows.
