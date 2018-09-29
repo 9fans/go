b359e14e18edeb0ca7011e76d3e89b90c9a736f6
Fazlul Shahriar <fshahriar@gmail.com>
2018-09-29 00:19:49 -0400

    plumb: use /mnt/plumb instead of trying to mount $plumbsrv in Plan 9

--- plumb/plumb_plan9.go
+++ plumb/plumb_plan9.go
@@ -1,22 +1,10 @@
 package plumb
 
 import (
-	"fmt"
-	"os"
-	"strings"
-
 	"9fans.net/go/plan9/client"
 )
 
 func mountPlumb() {
-	name := os.Getenv("plumbsrv")
-	if name == "" {
-		fsysErr = fmt.Errorf("$plumbsrv not set")
-		return
-	}
-	p := "/srv/"
-	if strings.HasPrefix(name, p) {
-		name = name[len(p):]
-	}
-	fsys, fsysErr = client.MountService(name)
+	fsys = &client.Fsys{Mtpt: "/mnt/plumb"}
+	fsysErr = nil
 }
