1bbb173c689caa7f7d756b1ae9e162a869167c54
Fazlul Shahriar <fshahriar@gmail.com>
2021-04-16 19:03:45 -0400

    plan9/client: make ReadAt compatible with io.ReaderAt interface (#26)
    Satisfy this requirement of io.ReaderAt:
    
        When ReadAt returns n < len(p), it returns a non-nil error
        explaining why more bytes were not returned.
    
    In other words, when EOF is reached and 0 < n < len(p), io.EOF error
    must be returned. ReadAt was delaying sending io.EOF until n == 0.
    
    Satisfying io.ReaderAt makes it possible to substitute Fid with os.File
    in Plan 9.

--- acme/acme.go
+++ acme/acme.go
@@ -10,6 +10,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -370,7 +371,7 @@ func (w *Win) ReadAddr() (q0, q1 int, err error) {
 	}
 	buf := make([]byte, 40)
 	n, err := f.ReadAt(buf, 0)
-	if err != nil {
+	if err != nil && err != io.EOF {
 		return 0, 0, err
 	}
 	a := strings.Fields(string(buf[0:n]))
